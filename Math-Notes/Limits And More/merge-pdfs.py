0d59fd94b87bd54c5b5517b4b412c63fc4da069a
Martin Matei <martin.matei@gmail.com>
2023-12-01 22:24:51 +0200

    Added Limits dir

---
+++ Math-Notes/Limits And More/merge-pdfs.py
@@ -0,0 +1,14 @@
+from pypdf import PdfMerger
+
+pdfs = []
+
+for i in range(1,11):
+    pdfs.append(f'{i}.pdf')
+
+merger = PdfMerger()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("result.pdf")
+merger.close()
